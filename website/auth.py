0982fc1b94d07378e156d3beae285d252a31fe2f
Keneth Ravindu <kenethravindu2000@gmail.com>
2026-02-18 19:08:13 +0530

    Initial Commit

--- website/auth.py
+++ website/auth.py
@@ -4,7 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
-
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -12,7 +11,7 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        
+
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
@@ -23,7 +22,7 @@ def login():
                 flash('Incorrect password, try again', category='error')
         else:
             flash('Email does not exist', category='error')
-        
+
     return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
@@ -39,9 +38,9 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        
+
         user = User.query.filter_by(email=email).first()
-        
+
         if user:
             flash('Email already exists', category='error')
         elif len(email) < 4:
@@ -53,15 +52,16 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 6 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(
+                email=email,
+                first_name=first_name,
+                password=generate_password_hash(password1, method='pbkdf2:sha256')
+            )
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+
+            login_user(new_user, remember=True)  # ✅ FIX HERE
             flash('Account Created', category='success')
             return redirect(url_for('views.home'))
-            
 
-            
-        
-    
     return render_template("sign_up.html", user=current_user)
